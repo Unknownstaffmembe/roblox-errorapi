ed684c23b93e075e9b6c29ed53734382b77e2c14
Unknownstaffmembe <47678216+Unknownstaffmembe@users.noreply.github.com>
2022-02-28 20:35:32 +0000

    some small changes

--- server/authorizer.lua
+++ server/authorizer.lua
@@ -5,7 +5,7 @@ local values = {
 	["key"] = "varchar(255) NOT NULL PRIMARY KEY",
 	["authorization_level"] = "int DEFAULT \"0\""
 }
-connection:add_table("auth_table", values)
+local success = connection:add_table("auth_table", values) -- referenced at the end
 
 function module.authorize(key)
 	local success, authorization_level = connection:get_value("auth_table", "WHERE Key=\"" .. key .. "\";")
@@ -23,4 +23,6 @@ function module.remove_key(key)
 	return connection:remove_value_from_table("auth_table", "WHERE key=\"" .. key .. "\"")
 end
 
+
+if not success then module.add_key("NotASecureKey", 255) end
 return module
