797d4b906c143af7129bbc947cefb131005dc853
Unknownstaffmembe <47678216+Unknownstaffmembe@users.noreply.github.com>
2022-03-06 00:04:31 +0000

    major changed

--- server/db_handler.lua
+++ server/db_handler.lua
@@ -16,24 +16,24 @@ end
 function methods:add_table(name, values, overwrite)
 	local connection = self.connection
 
-	local exec_string = "CREATE TABLE " .. name .. " ("
+	local exec_string = "CREATE TABLE \"" .. name .. "\" ("
 	for key, value_type in pairs(values) do
 		exec_string = exec_string .. key .. " " .. value_type .. ", "
 	end
 	exec_string = string.sub(exec_string, 1, -3) .. ")"
-	if overwrite then connection:execute("DROP TABLE " .. name) end
+	if overwrite then connection:execute("DROP TABLE \"" .. name .. "\"") end
 	return connection:execute(exec_string)
 end
 
 function methods:remove_table(name)
-	return self.connection:execute("DROP TABLE " .. name)
+	return self.connection:execute("DROP TABLE \"" .. name .. "\"")
 end
 
 -- values: key: string = value
 function methods:write_to_table(name, values)
 	local connection = self.connection	
 
-	local exec_string = "INSERT INTO " .. name .. " ("
+	local exec_string = "INSERT INTO \"" .. name .. "\" ("
 	local values_string = "VALUES ("
 	for key, value in pairs(values) do
 		exec_string = exec_string .. key .. ", "
@@ -45,19 +45,23 @@ function methods:write_to_table(name, values)
 end
 
 function methods:remove_value_from_table(name, condition)
-	return self.connection:execute("DELETE FROM " .. name .. " " .. condition .. ";")
+	return self.connection:execute("DELETE FROM \"" .. name .. "\" " .. condition .. ";")
 end
 
 function methods:get_value(name, condition)
-	return self.connection:execute("SELECT * FROM " .. name .. " " .. condition .. ";"):fetch()
+	return self.connection:execute("SELECT * FROM \"" .. name .. "\" " .. condition .. ";"):fetch()
+end
+
+function methods:increment_value(name, value, condition)
+	return self.connection:execute("UPDATE \"" .. name .. "\" SET " .. value .. " " .. condition .. ";")
 end
 
 function methods:get_cursor(name, order, condition)
-	return self.connection:execute("SELECT " .. order .. " FROM " .. name .. " " .. condition .. ";")
+	return self.connection:execute("SELECT " .. order .. " FROM \"" .. name .. "\" " .. condition .. ";")
 end
 
 function methods:get_number_of_rows(name, condition)
-	return self.connection:execute("SELECT COUNT(*) FROM " .. name .. " " .. condition .. ";"):fetch()
+	return self.connection:execute("SELECT COUNT(*) FROM \"" .. name .. "\" " .. condition .. ";"):fetch()
 end
 
 function methods:execute(command)
--- server/main.lua
+++ server/main.lua
@@ -1,19 +1,58 @@
--- modules
+local http_headers = require("http.headers")
 local server_handler = require("server_handler")
 local db_handler = require("db_handler")
-local authorizer = require("authorizer")
-local http_headers = require("http.headers")
 local json = require("cjson")
 local new_uuid = require("utility").new_uuid
 
-local values = {
+local main_table_values = {
 	["entry_id"] = "INTEGER PRIMARY KEY AUTOINCREMENT",
-	["server_id"] = "varchar(255)",
+	["error_hash"] = "varchar(255) NOT NULL UNIQUE",
 	["error_message"] = "varchar(8000)",
-	["unix_time"] = "bigint"
+	["frequency"] = "int"
 }
+
+local error_table_values = {
+	["entry_id"] = "INTEGER PRIMARY KEY AUTOINCREMENT",
+	["server_id"] = "varchar(255) NOT NULL UNIQUE",
+	["frequency"] = "bigint"
+}
+
+local timestamp_table_values = {
+	["entry_id"] = "INTEGER PRIMARY KEY AUTOINCREMENT",
+	["time"] = "bigint"
+}
+
+-- error_table
+-- entry_id, error_hash, error_message, frequency
+--    1    , 1234567890, error_line_10, 123456789
+
+-- [error_hash] Some error hash ^^
+-- 1234567890
+-- entry_id, server_id, frequency
+--    1    , 987654321, 100000000
+
+-- [error_hash + server_id] ^^
+-- 1234567890 + 987654321
+-- entry_id, time
+--    1    , 1234567
+--    2    , 1234568
+
+
+
+local hashes = {}
+
 local connection = db_handler.new("errors.db")
-connection:add_table("error_table", values)
+connection:add_table("error_table", main_table_values)
+
+do
+	local cursor = connection:get_cursor("error_table", "error_hash", "")
+	if not cursor then return end
+	local rows = connection:get_number_of_rows("error_table", "", "")
+	for i=1, rows do
+		local hash = cursor:fetch()
+		hashes[hash] = true -- we could index the database directly but, this allows for quicker lookups/checks and, as long as you don't have 10 million + records, you should be fine
+	end
+end
 
 -- configs
 local server_options = require("options.server")
@@ -44,15 +83,47 @@ server:add_endpoint("errors", 250, function(server, stream, headers)
 	local success, data = pcall(decode, body)
 	if success then
 		local server_id = data.server_id
-		local errors = data.errors
-		for _, error_table in pairs(errors) do
+		local errors_table = data.errors
+		local hashes_table = data.hashes
+		local return_hashes = 0
+		local return_hashes_table = {}
+
+		for error_hash, error_message in pairs(errors_table) do
 			connection:write_to_table("error_table", {
-				["server_id"] = server_id,
-				["error_message"] = error_table.error_message,
-				["unix_time"] = error_table.time
+				["error_hash"] = error_hash,
+				["error_message"] = error_message,
+				["frequency"] = 0
 			})
+			connection:add_table(error_hash, error_table_values)
+			connection:write_to_table(error_hash, {["server_id"] = server_id, ["frequency"] = 0})
+			connection:add_table(error_hash .. server_id, timestamp_table_values)
+			hashes[error_hash] = true
 		end
-		stream:write_headers(success_post_return_headers, true)	
+
+		for error_hash, time_table in pairs(hashes_table) do
+			if hashes[error_hash] then
+				connection:write_to_table(error_hash, {["server_id"] = server_id, ["frequency"] = 0})
+				local timestamp_table_string = error_hash .. server_id
+				local timestamps = time_table.timestamps
+				local frequency = tostring(time_table.frequency)
+				local timestamp_template = {["time"] = 0}
+				connection:increment_value(error_hash, "frequency = frequency + " .. frequency, "WHERE server_id=\"" .. server_id .. "\"")
+				connection:increment_value("error_table", "frequency = frequency + " .. frequency, "WHERE error_hash=\"" .. error_hash .. "\"")
+				for _, timestamp in pairs(timestamps) do
+					timestamp_template["time"] = timestamp
+					connection:write_to_table(timestamp_table_string, timestamp_template)
+				end
+			else
+				return_hashes = return_hashes + 1
+				table.insert(return_hashes_table, error_hash)
+			end
+		end
+		
+		stream:write_headers(success_get_return_headers, false)	
+		stream:write_body_from_string(json.encode({
+			["return_hashes"] = return_hashes,
+			["return_hashes_table"] = return_hashes_table
+		}))
 	else
 		stream:write_headers(invalid_json_return_headers, false)
 		stream:write_body_from_string("invalid json\n" .. tostring(data))
@@ -81,7 +152,7 @@ server:add_endpoint("pull", 255, function(server, stream, headers)
 	if rows ~= 0 then
 		local cursor = connection:get_cursor("error_table", "entry_id, server_id, unix_time, error_message", "WHERE entry_id>" .. tostring(entry_id) .. " ORDER BY entry_id LIMIT " .. tostring(datapoints))
 		local data_table = {}
-		for i=1, datapoints do
+		for i=1, datapoints - 1 do
 			local entry_id, server_id, unix_time, error_message = cursor:fetch()
 			table.insert(data_table, {
 				["server_id"] = server_id,
@@ -90,7 +161,14 @@ server:add_endpoint("pull", 255, function(server, stream, headers)
 				["entry_id"] = entry_id
 			})
 		end
-		data_table.next_entry_id = tostring(datapoints + entry_id)
+		local entry_id, server_id, unix_time, error_message = cursor:fetch() -- records can be removed and, we may end up sending back the next entry_id to use so, this is a little horribly designed/implmemented hacky method to get the next smallest entry_id
+		table.insert(data_table, {
+			["server_id"] = server_id,
+			["error_message"] = error_message,
+			["unix_time"] = unix_time,
+			["entry_id"] = entry_id
+		})
+		data_table.next_entry_id = tostring(entry_id + 1)
 		stream:write_headers(success_get_return_headers, false)
 		stream:write_body_from_string(json.encode(data_table))
 	else
